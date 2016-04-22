0664d6cd83b96aeeeb32fb3aa72b09123172e259
Javier Roucher <jroucher@tcpsi.es>
2016-04-22 12:56:51 +0200

    pwd where i am

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -56,11 +56,12 @@ echo "This is a script to run on a Deploy"
 # fi
 # netstat -putan
 virtualenv venv
+pwd
+source /home/contint/workspace/vdc/portalesnovdc/venv/bin/activate
 sudo pip install -r requirements.txt --ignore-installed
 wget https://github.com/jgraham/wires/releases/download/v0.6.2/wires-0.6.2-linux64.gz /home/contint/workspace/vdc/portalesnovdc/
 gzip -d /home/contint/workspace/vdc/portalesnovdc/wires-0.6.2-linux64.gz
 export PATH=$PATH:/home/contint/workspace/vdc/portalesnovdc/
-source /home/contint/workspace/vdc/portalesnovdc/venv/bin/activate
 
 # ---------------------------------------------------------
 # DOWNLOAD
